6e5b7105448bc95ecda345d4a88be89690b4d8a1
Joe <efessel@gmail.com>
2017-02-18 20:36:42 -0500

    initial project setup

---
+++ src/app/components/test.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Hello world, {{msg}}!
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      msg: 'I\'m a vue template!'
+    }
+  },
+  name: 'test'
+}
+</script>
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/app/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    loaders: [
+      {
+        // Ask webpack to check: If this file ends with .js, then apply some transforms
+        test: /\.js$/,
+        // Transform it with babel
+        loader: 'babel-loader',
+        // don't transform node_modules folder (which don't need to be compiled)
+        exclude: /node_modules/
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
+  }
+};
